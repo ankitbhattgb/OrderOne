fcfae035cc55643b53c69c8e515043fbcecdec5f
Ankit Bhatt <ankit.bhattgb@gmail.com>
2018-05-13 15:48:47 +0530

    new commit

--- src/test/java/RunCucumberTest.java
+++ src/test/java/RunCucumberTest.java
@@ -1,5 +1,3 @@
-
-
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
@@ -7,8 +5,8 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-		plugin={"pretty","html:target/cucumber-html-report"}, strict = true, monochrome = true
-		,features = {"src/test/java"}
+		plugin= {"pretty","html:target/cucumber-html-report"}, strict = true, monochrome = true,
+		features="src/test/java"
 		)
 
 public class RunCucumberTest {
